515ed4a78cdc616cdc618ab33defffd0d0a55c60
Vlad Kovalchuk <vlad@vivchip.com>
2025-01-07 08:49:26 -0800

    overage grade

--- tmp_oop.py
+++ tmp_oop.py
@@ -1,14 +1,44 @@
+class Student:
+    def __init__(self, name , age, grade):
+        self.name =name
+        self.age =age
+        self.grade = grade # 0 - 100
 
-class Dog:
-    """ dirty animal """
-    def meow(self):
-        """ meow """
-        return "meow"
-    def bark(self):
-        """ bark"""
-        print('bark')
+    def get_grade(self):
+        return self.grade
 
-boris = Dog()
-print(type(boris))
-boris.bark()
-print(boris.meow())
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        #print('overload')
+        return False
+
+    def get_average_grade(self):
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+        return value / len(self.students)
+
+st1 = Student('First', 18, 55)
+st2 = Student('Second', 19, 66)
+st3 = Student('Third', 91, 99)
+course_a  = Course('Math', 2)
+course_a.add_student(st1)
+print(course_a)
+print(course_a.students[0].name)
+
+
+course_a.add_student(st2)
+print(course_a.students[1].name)
+
+course_a.add_student(st3)
+print(course_a.get_average_grade())
+#print(course_a.students[2].name)
+
